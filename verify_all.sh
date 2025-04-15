082ea70dcea88d287c13f1c26345b7c5a6b3d55a
Maarten Derickx <mderickx@euler.math.hr>
2025-04-15 15:05:44 +0200

    Add timing and memory information for the sage scripts

--- verify_all.sh
+++ verify_all.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 ls *.m | parallel --joblog logs/magma_verify_joblog.txt -j ${1:-10} 'magma -n {} >| logs/{.}.txt'
-ls *.sage | parallel --joblog logs/sage_verify_joblog.txt -j ${1:-10} 'sage {} >| logs/{.}.txt'
+ls *.sage | parallel --joblog logs/sage_verify_joblog.txt -j ${1:-10} 'command time -o logs/{.}.timing.txt -v sage {} >| logs/{.}.txt'
